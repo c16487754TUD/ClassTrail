73cf663b3a66f6d9fcfcf37bd4d716b4ceafa833
unknown <c16487754@mydit.ie>
2022-02-16 12:31:14 +0000

    added studenthub and fixed urls

--- FYP/classtrail2/classtrail2/accounts/urls.py
+++ FYP/classtrail2/classtrail2/accounts/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('teacher_hub/', views.teacher_hub, name = 'teacher_hub'),
+    path('student_hub/', views.student_hub, name = 'student_hub'),
     path('manage_classes', views.manage_classes, name = 'manage_classes'),
 ]
--- FYP/classtrail2/classtrail2/accounts/views.py
+++ FYP/classtrail2/classtrail2/accounts/views.py
@@ -13,6 +13,9 @@ def register(request):
 def teacher_hub(request):
     return render(request, '../templates/teacher_hub.html')
 
+def student_hub(request):
+    return render(request, '../templates/student_hub.html')
+
 def manage_classes(request):
     return render(request, '../templates/manage_classes.html')
 
